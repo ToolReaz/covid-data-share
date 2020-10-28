28c2db3d52ff7fa8a4593f88bb29425ec29e7e27
ToolReaz <toolreaz@outlook.fr>
2020-10-28 15:49:09 +0100

    Format

--- App.js
+++ App.js
@@ -1,34 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Text, View } from "react-native";
-import * as Font from 'expo-font';
+import * as Font from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./src/screens/HomeScreen";
-import  StoreScreen  from "./src/screens/StoreScreen";
-import  ShareScreen  from "./src/screens/ShareScreen";
-import  ScanScreen  from "./src/screens/ScanScreen";
-import  CreateProfileScreen from "./src/screens/CreateProfileScreen";
+import StoreScreen from "./src/screens/StoreScreen";
+import ShareScreen from "./src/screens/ShareScreen";
+import ScanScreen from "./src/screens/ScanScreen";
+import CreateProfileScreen from "./src/screens/CreateProfileScreen";
 
 const Stack = createStackNavigator();
 
-
-
 export default function App() {
-
   const [loaded] = Font.useFonts({
-    RobotoBlack: require('./assets/fonts/Roboto-Black.ttf'),
-    RobotoBlackItalic: require('./assets/fonts/Roboto-BlackItalic.ttf'),
-    RobotoBold: require('./assets/fonts/Roboto-Bold.ttf'),
-    RobotoBoldItalic: require('./assets/fonts/Roboto-BoldItalic.ttf'),
-    RobotoItalic: require('./assets/fonts/Roboto-Italic.ttf'),
-    RobotoLight: require('./assets/fonts/Roboto-Light.ttf'),
-    RobotoLightItalic: require('./assets/fonts/Roboto-LightItalic.ttf'),
-    RobotoMedium: require('./assets/fonts/Roboto-Medium.ttf'),
-    RobotoMediumItalic: require('./assets/fonts/Roboto-MediumItalic.ttf'),
-    RobotoRegular: require('./assets/fonts/Roboto-Regular.ttf'),
-    RobotoThin: require('./assets/fonts/Roboto-Thin.ttf'),
-    RobotoThinItalic: require('./assets/fonts/Roboto-ThinItalic.ttf'),
+    RobotoBlack: require("./assets/fonts/Roboto-Black.ttf"),
+    RobotoBlackItalic: require("./assets/fonts/Roboto-BlackItalic.ttf"),
+    RobotoBold: require("./assets/fonts/Roboto-Bold.ttf"),
+    RobotoBoldItalic: require("./assets/fonts/Roboto-BoldItalic.ttf"),
+    RobotoItalic: require("./assets/fonts/Roboto-Italic.ttf"),
+    RobotoLight: require("./assets/fonts/Roboto-Light.ttf"),
+    RobotoLightItalic: require("./assets/fonts/Roboto-LightItalic.ttf"),
+    RobotoMedium: require("./assets/fonts/Roboto-Medium.ttf"),
+    RobotoMediumItalic: require("./assets/fonts/Roboto-MediumItalic.ttf"),
+    RobotoRegular: require("./assets/fonts/Roboto-Regular.ttf"),
+    RobotoThin: require("./assets/fonts/Roboto-Thin.ttf"),
+    RobotoThinItalic: require("./assets/fonts/Roboto-ThinItalic.ttf"),
   });
 
   if (!loaded) {
