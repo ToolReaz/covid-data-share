c8b336069270031ac9be82cc3c3ecd57f6ca2de0
ToolReaz <toolreaz@outlook.fr>
2020-11-06 19:26:31 +0100

    Design splahs screen

--- src/screens/SplashScreen.jsx
+++ src/screens/SplashScreen.jsx
@@ -1,12 +1,49 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from "react";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { COLORS } from "../styles/colors";
 
 export default class SplashScreen extends Component {
-    render() {
-        return (
-            <View>
-                <Text> SplashScreen </Text>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={s.container}>
+        <View style={s.subContainer}>
+          <Text style={s.title}>CODASH</Text>
+          <View style={s.underline}></View>
+        </View>
+        <View style={s.loader}>
+          <ActivityIndicator size="large" color={COLORS.Primary} />
+        </View>
+      </View>
+    );
+  }
 }
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+
+  subContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
+
+  title: {
+    fontSize: 36,
+    fontFamily: "RobotoLight",
+    color: COLORS.Dark,
+    textAlign: "center",
+  },
+
+  underline: {
+    width: "20%",
+    alignSelf: "center",
+    borderTopColor: COLORS.Primary,
+    borderTopWidth: 2,
+  },
+
+  loader: {
+    flex: 1,
+  },
+});
