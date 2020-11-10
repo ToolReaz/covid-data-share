5efc19712268165a013f80c71685665ce4f91c63
ToolReaz <toolreaz@outlook.fr>
2020-11-10 15:40:21 +0100

    Delete font imports & expo fonts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import * as Font from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator, createSw } from "@react-navigation/stack";
 import HomeScreen from "./screens/HomeScreen";
@@ -7,7 +6,6 @@ import StoreScreen from "./screens/StoreScreen";
 import ShareScreen from "./screens/ShareScreen";
 import ScanScreen from "./screens/ScanScreen";
 import CreateProfileScreen from "./screens/CreateProfileScreen";
-import SplashScreen from "./screens/SplashScreen";
 import { COLORS } from "./styles/colors";
 import { t } from "i18n-js";
 import InitWelcomeScreen from "./screens/initialization/InitWelcomeScreen";
@@ -38,25 +36,6 @@ export default function App() {
       });
     },
   }));
-
-  const [fontLoaded] = Font.useFonts({
-    RobotoBlack: require("../assets/fonts/Roboto-Black.ttf"),
-    RobotoBlackItalic: require("../assets/fonts/Roboto-BlackItalic.ttf"),
-    RobotoBold: require("../assets/fonts/Roboto-Bold.ttf"),
-    RobotoBoldItalic: require("../assets/fonts/Roboto-BoldItalic.ttf"),
-    RobotoItalic: require("../assets/fonts/Roboto-Italic.ttf"),
-    RobotoLight: require("../assets/fonts/Roboto-Light.ttf"),
-    RobotoLightItalic: require("../assets/fonts/Roboto-LightItalic.ttf"),
-    RobotoMedium: require("../assets/fonts/Roboto-Medium.ttf"),
-    RobotoMediumItalic: require("../assets/fonts/Roboto-MediumItalic.ttf"),
-    RobotoRegular: require("../assets/fonts/Roboto-Regular.ttf"),
-    RobotoThin: require("../assets/fonts/Roboto-Thin.ttf"),
-    RobotoThinItalic: require("../assets/fonts/Roboto-ThinItalic.ttf"),
-  });
-
-  if (!fontLoaded) {
-    return <SplashScreen />;
-  }
 
   const AppStack = (
     <>
